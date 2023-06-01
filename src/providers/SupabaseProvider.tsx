8b8c86b77a69febe41dbd1136e75810fe6b82be2
Navin Avadhani <knavinavadhani@gmail.com>
2023-06-01 22:45:58 +0530

    Added supabase auth providers.

---
+++ src/providers/SupabaseProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+// React and Next.
+import { useState } from "react";
+
+// External packages.
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+// Types.
+import { Database } from "../../types_db";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+}
+
+export default function SupabaseProvider({ children }: SupabaseProviderProps) {
+  const [supabaseClient] = useState(() =>
+    createClientComponentClient<Database>()
+  );
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+}
---
+++ src/providers/UserProvider.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+// Hooks.
+import { MyUserContextProvider } from "@/hooks/useUser";
+
+interface UserProviderProps {
+  children: React.ReactNode;
+}
+
+export default function UserProvider({ children }: UserProviderProps) {
+  return <MyUserContextProvider>{children}</MyUserContextProvider>;
+}
